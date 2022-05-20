fd772553df3464a8a7166c8ca59150734d0ec40b
Sivalingam <sivalingam160599@gmail.com>
2022-05-20 13:12:47 +0530

    smallchange

--- src/stackprogram/DescStack.java
+++ src/stackprogram/DescStack.java
@@ -3,7 +3,6 @@ package stackprogram;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class DescStack {
 
@@ -12,33 +11,33 @@ public class DescStack {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the array size: ");
 		int size  = input.nextInt();
-		List<Integer> stackEmty = new ArrayList<Integer>(size);
-		List<Integer> stackfirst = new ArrayList<Integer>(size);
+		List<Integer> listEmty = new ArrayList<Integer>(size);
+		List<Integer> listfirst = new ArrayList<Integer>(size);
 
 		System.out.println("Enter the StackValue  :");
 		for(int i =0 ; i<size;  i++)
 		{
-			stackfirst.add(input.nextInt());
+			listfirst.add(input.nextInt());
 		}    
 		int count = 0;
-		for (int i = 0; i < stackfirst.size()-1; i++) 
+		for (int i = 0; i < listfirst.size()-1; i++) 
 		{
-			if (stackfirst.get(i) > stackfirst.get(i + 1)) 
+			if (listfirst.get(i) > listfirst.get(i + 1)) 
 			{	
-				stackEmty.add(stackfirst.get(i));	
+				listEmty.add(listfirst.get(i));	
 			} 
 			else
 			{ 	
-				stackfirst.remove(stackfirst.get(i + 1)); 
-				stackEmty.add(stackfirst.get(i));
+				listfirst.remove(listfirst.get(i + 1)); 
+				listEmty.add(listfirst.get(i));
 			}
 			count++;
 		}
 		System.out.println(count-1);
 
-		for (int i = 0; i < stackEmty.size(); i++) 
+		for (int i = 0; i < listEmty.size(); i++) 
 		{
-			System.out.println("The Stack value is " + stackEmty.get(i) + " ");
+			System.out.println("The Stack value is " + listEmty.get(i) + " ");
 		}
 	}
 
