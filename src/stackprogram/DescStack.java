df7158bc0a287062e1e9dbaab63c0d60b68029a6
Sivalingam <sivalingam160599@gmail.com>
2022-05-20 12:56:28 +0530

    Java two program

--- src/stackprogram/DescStack.java
+++ src/stackprogram/DescStack.java
@@ -1,47 +1,45 @@
 package stackprogram;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class DescStack {
-	
-	public static int[] addMultiple(int[] a, Stack stack) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != ' ') {
-                stack.push(a[i]);
-            }
-        }
-        return a;
-    }
 
 	public static void main(String[] args)
 	{
-		int[] a = new int[]{6, 5, 8, 4, 7, 10, 9};
-        Stack<Integer> stackEmty = new Stack<>();
-        Stack<Integer> stackfirst = new Stack<>();
-        addMultiple(a, stackfirst);
-       
-        
-        int count = 0;
-        for (int i = 0; i < stackfirst.size() - 1; i++) 
-        {
-            if (stackfirst.get(i) > stackfirst.get(i + 1)) 
-            {
-            	stackEmty.add(stackfirst.get(i));
-            	stackfirst.pop();
-            } else
-            {
-            	stackEmty.add(stackfirst.get(i));
-            	stackfirst.remove(stackfirst.get(i + 1));
-            }
-            count++;
-        }
-        System.out.println(count-1);
-        
-        for (int i = 0; i < stackEmty.size(); i++) 
-        {
-            System.out.println("The elements in the stack are " + stackEmty.get(i) + " ");
-        }
-           
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the array size: ");
+		int size  = input.nextInt();
+		List<Integer> stackEmty = new ArrayList<Integer>(size);
+		List<Integer> stackfirst = new ArrayList<Integer>(size);
+
+		System.out.println("Enter the StackValue  :");
+		for(int i =0 ; i<size;  i++)
+		{
+			stackfirst.add(input.nextInt());
+		}    
+		int count = 0;
+		for (int i = 0; i < stackfirst.size()-1; i++) 
+		{
+			if (stackfirst.get(i) > stackfirst.get(i + 1)) 
+			{	
+				stackEmty.add(stackfirst.get(i));	
+			} 
+			else
+			{ 	
+				stackfirst.remove(stackfirst.get(i + 1)); 
+				stackEmty.add(stackfirst.get(i));
+			}
+			count++;
+		}
+		System.out.println(count-1);
+
+		for (int i = 0; i < stackEmty.size(); i++) 
+		{
+			System.out.println("The Stack value is " + stackEmty.get(i) + " ");
+		}
 	}
 
 }
